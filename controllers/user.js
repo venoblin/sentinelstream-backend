85379f8a1e799cdaa111feb788c9166f6bab15a7
Jonathan <villagomezhjonathan@gmail.com>
2026-02-20 16:09:12 -0500

    removed password hash from results

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,13 @@ controllers.getAllUsers = async (req, res) => {
   try {
     const users = await repo.findAllUsers(req.query)
 
-    return res.status(200).json({ users: users })
+    const cleanedUsers = users.map((user) => {
+      delete user.dataValues.passwordHash
+
+      return user
+    })
+
+    return res.status(200).json({ users: cleanedUsers })
   } catch (error) {
     return res.status(500).json({ error: error.message })
   }
@@ -17,6 +23,8 @@ controllers.getUserById = async (req, res) => {
     const { id } = req.params
 
     const user = await repo.findUserById(id)
+
+    delete user.dataValues.passwordHash
 
     return res.status(200).json({ user: user })
   } catch (error) {
