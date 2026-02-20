37c4ad170bb3f5e8b7317b1cf0eb5358c9176eb1
Jonathan <villagomezhjonathan@gmail.com>
2026-02-20 16:19:19 -0500

    created authenticaion middleware

---
+++ middleware/index.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
+const APP_SECRET = process.env.APP_SECRET
+
+const middleware = {}
+
+middleware.hashPassword = (password) => {
+  const hash = bcrypt.hash(password, SALT_ROUNDS)
+  return hash
+}
+
+middleware.comparePassword = (password, storedPassword) => {
+  const match = bcrypt.compare(password, storedPassword)
+  return match
+}
+
+middleware.createToken = (payload) => {
+  const token = jwt.sign(payload, APP_SECRET)
+  return token
+}
+
+middleware.verifyToken = (req, res, next) => {
+  const { token } = res.locals
+  try {
+    const payload = jwt.verify(token, APP_SECRET)
+    if (payload) {
+      res.locals.payload = payload
+      return next()
+    }
+    res.status(401).json({ status: 'Error', msg: 'Unauthorized' })
+  } catch (err) {
+    res.status(401).json({ status: 'Error', msg: 'Unauthorized' })
+  }
+}
+
+middleware.stripToken = (req, res, next) => {
+  try {
+    const token = req.headers['authorization'].split(' ')[1]
+    if (token) {
+      res.locals.token = token
+      return next()
+    }
+  } catch (err) {
+    res.status(401).json({ status: 'Error', msg: 'Unauthorized' })
+  }
+}
+
+module.exports = middleware
