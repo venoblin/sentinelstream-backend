5fbae20a0e3def6e9d5e0453c0f6896f63376e7b
Jonathan <villagomezhjonathan@gmail.com>
2026-02-21 01:41:54 -0500

    worked on seeders

--- seeders/usersSeed.js
+++ seeders/usersSeed.js
@@ -1,1 +1,15 @@
-const repo = require('../repositories/user')
+const { faker } = require('@faker-js/faker')
+
+const user = {
+  email: faker.internet.email(),
+  password: 'password',
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  locationCity: faker.location.city(),
+  locationCountry: faker.location.country(),
+  riskScore: 12,
+  role: 'analyst',
+  isActive: true
+}
+
+console.log(user)
