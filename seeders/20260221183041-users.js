51b8061182a13ed59291bda8957953a29d688b5f
Jonathan <villagomezhjonathan@gmail.com>
2026-02-21 15:28:49 -0500

    finished seeders

--- seeders/20260221183041-users.js
+++ seeders/20260221183041-users.js
@@ -1,13 +1,16 @@
 'use strict'
 /** @type {import('sequelize-cli').Migration} */
-
+require('dotenv').config()
 const { faker } = require('@faker-js/faker')
 const bcrypt = require('bcrypt')
+
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
 
 module.exports = {
   async up(queryInterface, Sequelize) {
     const users = []
-    const hash = await bcrypt.hash('password123', process.env.SALT_ROUNDS)
+
+    const hash = await bcrypt.hash('password123', SALT_ROUNDS)
 
     for (let i = 0; i < 10; i++) {
       users.push({
@@ -19,14 +22,16 @@ module.exports = {
         locationCountry: faker.location.country(),
         riskScore: faker.number.int({ min: 1, max: 100 }),
         role: i % 2 === 0 ? 'user' : 'analyst',
-        isActive: true
+        isActive: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
       })
     }
 
-    await queryInterface.bulkInsert('Users', users, {})
+    await queryInterface.bulkInsert('users', users, {})
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Users', null, {})
+    await queryInterface.bulkDelete('users', null, {})
   }
 }
