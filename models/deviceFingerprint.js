f2b8cc6d9fd678fc632d22a0d923984008cb1890
Jonathan <villagomezhjonathan@gmail.com>
2026-02-22 01:32:00 -0500

    added table and model options

--- models/deviceFingerprint.js
+++ models/deviceFingerprint.js
@@ -3,32 +3,39 @@ const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class DeviceFingerprint extends Model {}
 
-  DeviceFingerprint.init({
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+  DeviceFingerprint.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      deviceHash: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      ipAddress: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      userAgent: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      trustScore: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      isBanned: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+      }
     },
-    deviceHash: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    ipAddress: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    userAgent: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    trustScore: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    isBanned: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false
+    {
+      sequelize,
+      modelName: 'DeviceFingerprint',
+      tableName: 'deviceFingerprints'
     }
-  })
+  )
 
   DeviceFingerprint.associate = (models) => {
     DeviceFingerprint.belongsTo(models.User, {
--- models/transaction.js
+++ models/transaction.js
@@ -3,42 +3,49 @@ const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Transaction extends Model {}
 
-  Transaction.init({
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+  Transaction.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      amount: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+      },
+      currency: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      merchant: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      locationCity: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      locationCountry: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      status: {
+        type: DataTypes.ENUM(
+          'PENDING',
+          'APPROVED',
+          'FLAGGED',
+          'BLOCKED',
+          'REVERSED'
+        ),
+        allowNull: false
+      }
     },
-    amount: {
-      type: DataTypes.DECIMAL,
-      allowNull: false
-    },
-    currency: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    merchant: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    locationCity: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    locationCountry: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    status: {
-      type: DataTypes.ENUM(
-        'PENDING',
-        'APPROVED',
-        'FLAGGED',
-        'BLOCKED',
-        'REVERSED'
-      ),
-      allowNull: false
+    {
+      sequelize,
+      modelName: 'Transaction',
+      tableName: 'transactions'
     }
-  })
+  )
 
   Transaction.associate = (models) => {
     Transaction.hasMany(models.AuditLog, {
