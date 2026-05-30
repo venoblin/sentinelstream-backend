7930ad766577d828f86d33cfe7253f3c8067d099
Jonathan <villagomezhjonathan@gmail.com>
2026-05-30 18:16:34 -0400

    new commit of files

--- src/main/java/venoblin/SentinelstreamApplication.java
+++ src/main/java/venoblin/SentinelstreamApplication.java
@@ -1,13 +1,15 @@
-package com.example.demo;
+package venoblin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SentinelstreamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(SentinelstreamApplication.class, args);
+
+		System.out.println("Hello")
 	}
 
 }
--- src/test/java/venoblin/SentinelstreamApplicationTests.java
+++ src/test/java/venoblin/SentinelstreamApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package venoblin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class SentinelstreamApplicationTests {
 
 	@Test
 	void contextLoads() {
